f18df31a9422038ee2ba27a20602622ce688848e
Jeremy DELOISON <jdeloison@access-it.fr>
2021-09-18 00:08:38 +0200

    secure api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({readOnly: true, bodyParser: true});
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
